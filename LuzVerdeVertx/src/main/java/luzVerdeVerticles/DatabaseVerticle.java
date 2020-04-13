54751a09747a9c2809f42eb29c7dd96cc46b69b1
Alberto Naranjo <48420916+AlbertoNR98@users.noreply.github.com>
2020-04-13 18:37:17 +0200

    Subida DatabaseVerticle

---
+++ LuzVerdeVertx/src/main/java/luzVerdeVerticles/DatabaseVerticle.java
@@ -0,0 +1,37 @@
+package luzVerdeVerticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+
+//Verticle para gestionar la BBDD a través de Vert.x
+public class DatabaseVerticle extends AbstractVerticle{
+
+	private MySQLPool mySQLPool;
+	
+	@Override
+	public void start(Promise<Void> startPromise) {
+		//Conexión BBDD
+		MySQLConnectOptions mySQLConnectOptions = new MySQLConnectOptions().setPort(3306).setHost("localhost")
+				.setDatabase("luzverde").setUser("root").setPassword("zeus");
+		PoolOptions poolOptions = new PoolOptions().setMaxSize(15);
+		
+		mySQLPool = MySQLPool.pool(vertx, mySQLConnectOptions, poolOptions);
+		
+		//HTTP Server
+		Router router = Router.router(vertx);
+		vertx.createHttpServer().requestHandler(router::handle).listen(8082, result -> {
+			if(result.succeeded()) {
+				startPromise.complete();
+			}else {
+				startPromise.fail(result.cause());
+			}
+		});
+		
+		//Peticiones
+		
+	}
+}
