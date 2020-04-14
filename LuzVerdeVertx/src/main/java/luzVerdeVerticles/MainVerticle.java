1482db2dabb04902af3c8b789db1645a74bc2023
Alberto Naranjo <48420916+AlbertoNR98@users.noreply.github.com>
2020-04-14 16:59:17 +0200

    Actualización MainVerticle

--- LuzVerdeVertx/src/main/java/luzVerdeVerticles/MainVerticle.java
+++ LuzVerdeVertx/src/main/java/luzVerdeVerticles/MainVerticle.java
@@ -2,24 +2,12 @@ package luzVerdeVerticles;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.http.HttpServerRequest;
 
-
+//Verticle principal.
 public class MainVerticle extends AbstractVerticle{
-	@SuppressWarnings("deprecation")
 	@Override
 	public void start(Future<Void> startFuture) {
-		vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-			public void handle(HttpServerRequest request) {
-				request.response().end("<h1>LUZ VERDE</h1>Semáforos");
-			}
-		}).listen(8080, result -> {	//Puerto
-			if(result.succeeded()) {	//Saca el mensaje por consola si se ha iniciado correctamente
-				System.out.println("Todo correcto");
-			}else {
-				System.out.println(result.cause());
-			}
-		});
+		//Despliegue de API REST
+		vertx.deployVerticle(DatabaseVerticle.class.getName());
 	}
 }
