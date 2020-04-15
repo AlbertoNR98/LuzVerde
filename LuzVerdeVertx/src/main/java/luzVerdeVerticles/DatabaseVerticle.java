bd3f3d5dfbc074ff5d88b834b63099b06e377e47
Alberto Naranjo <48420916+AlbertoNR98@users.noreply.github.com>
2020-04-15 17:15:05 +0200

    Operaciones de usuario

--- LuzVerdeVertx/src/main/java/luzVerdeVerticles/DatabaseVerticle.java
+++ LuzVerdeVertx/src/main/java/luzVerdeVerticles/DatabaseVerticle.java
@@ -5,30 +5,34 @@ import luzVerdeTipos.Cruce;
 import luzVerdeTipos.LuzSemaforo;
 import luzVerdeTipos.Semaforo;
 import luzVerdeTipos.Sensor;
+import luzVerdeTipos.Usuario;
 import luzVerdeTipos.ValorSensorContaminacion;
 import luzVerdeTipos.ValorSensorTempHum;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.mysqlclient.MySQLClient;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
 import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
 
 
-//Verticle para gestionar la BBDD a travÃ©s de Vert.x
+//Verticle para gestionar la BBDD a través de Vert.x
 public class DatabaseVerticle extends AbstractVerticle{
 
 	private MySQLPool mySQLPool;
 	
 	@Override
 	public void start(Promise<Void> startPromise) {
-		//ConexiÃ³n BBDD
+		//Conexión BBDD
 		MySQLConnectOptions mySQLConnectOptions = new MySQLConnectOptions().setPort(3306).setHost("localhost")
 				.setDatabase("luzverde").setUser("root").setPassword("zeus");
 		PoolOptions poolOptions = new PoolOptions().setMaxSize(15);
@@ -39,15 +43,24 @@ public class DatabaseVerticle extends AbstractVerticle{
 		Router router = Router.router(vertx);
 		vertx.createHttpServer().requestHandler(router::handle).listen(8082, result -> {
 			if(result.succeeded()) {
-				System.out.println("Todo OK");
+				System.out.println("Todo OK. Sistema desplegado");
 				startPromise.complete();
 			}else {
+				System.out.println("Error al desplegar el sistema");
 				startPromise.fail(result.cause());
 			}
 		});
 		
 		//Peticiones
-		//router.route("links").blockingHandler(BodyHandler.create());
+			//Usuario
+		router.route("/api/usuarios").handler(BodyHandler.create());
+		
+		router.get("/api/usuarios/:idUsuario").handler(this::getUserByID);
+		router.get("/api/usuarios").handler(this::getAllUsers);
+		router.post("/api/usuarios").handler(this::postUser);
+		router.put("/api/usuarios/:idUsuario").handler(this::updateUserByID);
+		router.delete("/api/usuarios/:idUsuario").handler(this::deleteUserByID);
+		
 		router.get("/api/valor_sensor_contaminacion/values/:idSensor").handler(this::getValueBySensor);
 		router.get("/api/valor_sensor_temp_hum/values/:idSensor").handler(this::getValueBySensor2);
 		router.get("/api/sensor/sensors/:idSemaforo").handler(this::getSensorBySemaforo);
@@ -57,6 +70,97 @@ public class DatabaseVerticle extends AbstractVerticle{
 		
 	}
 	
+	//Operaciones
+		//Usuario
+	private void getAllUsers(RoutingContext routingContext) {
+		mySQLPool.query("SELECT * FROM luzverde.usuario", res ->{
+			if(res.succeeded()) {
+				RowSet<Row> resultSet = res.result();
+				System.out.println(resultSet.size()+" elementos obtenidos");
+				JsonArray result = new JsonArray();
+				for(Row row : resultSet) {
+					result.add(JsonObject.mapFrom(new Usuario(row.getInteger("idUsuario"), row.getString("nombre"), row.getString("apellidos"), row.getString("dni"), row.getInteger("fnacimiento"))));
+				}
+				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json").end(result.encodePrettily());						
+
+			}else {
+				routingContext.response().setStatusCode(401).putHeader("content-type", "application/json").end((JsonObject.mapFrom(res.cause()).encodePrettily()));
+				System.out.println("Error al hacer la operación");
+			}
+		});
+	}
+	
+	private void getUserByID(RoutingContext routingContext) {
+		mySQLPool.query("SELECT * FROM luzverde.usuario WHERE idUsuario = "+ routingContext.request().getParam("idUsuario"), res ->{
+			if(res.succeeded()) {
+				RowSet<Row> resultSet = res.result();
+				System.out.println(resultSet.size()+" elementos obtenidos");
+				JsonArray result = new JsonArray();
+				for(Row row : resultSet) {
+					result.add(JsonObject.mapFrom(new Usuario(row.getInteger("idUsuario"), row.getString("nombre"), row.getString("apellidos"), row.getString("dni"), row.getInteger("fnacimiento"))));
+				}
+				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json").end(result.encodePrettily());						
+
+			}else {
+				System.out.println("Error al hacer la operación\n");
+				routingContext.response().setStatusCode(401).putHeader("content-type", "application/json").end((JsonObject.mapFrom(res.cause()).encodePrettily()));
+			}
+		});
+	}
+	
+	private void postUser(RoutingContext routingContext) {
+		Usuario user = Json.decodeValue(routingContext.getBodyAsString(), Usuario.class);
+		mySQLPool.preparedQuery(
+				"INSERT INTO luzverde.usuario (idUsuario, nombre, apellidos, dni, fnacimiento) VALUES (?,?,?,?,?)",
+				Tuple.of(user.getIdUsuario(), user.getNombre(), user.getApellidos(), user.getApellidos(), user.getDni(), user.getFnacimiento()),
+				res -> {
+					if (res.succeeded()) {
+						System.out.println(res.result().rowCount()+"filas insertadas");
+						
+						long id = res.result().property(MySQLClient.LAST_INSERTED_ID);
+						user.setIdUsuario((int) id);
+						
+						routingContext.response().setStatusCode(200).putHeader("content-type", "application/json")
+								.end(JsonObject.mapFrom(user).encodePrettily());
+					} else {
+						System.out.println("Error al hacer la operación\n");
+						System.out.println(res.cause().toString());
+						routingContext.response().setStatusCode(401).putHeader("content-type", "application/json")
+								.end((JsonObject.mapFrom(res.cause()).encodePrettily()));
+					}
+				});
+	}
+	
+	private void updateUserByID(RoutingContext routingContext) {
+		JsonObject body = routingContext.getBodyAsJson();
+		
+		mySQLPool.query("UPDATE luzverde.usuario SET idUsuario = " + body.getInteger("idUsuario") + ", nombre = " + body.getString("nombre") + 
+				", apellidos = " + body.getString("apellidos") + ", dni = '" + body.getString("dni") + "', fnacimiento = '" + body.getInteger("fnacimiento")+" WHERE idUsuario = " + routingContext.request().getParam("idUsuario"),
+				res -> {
+			
+			if(res.succeeded()) {
+				System.out.println("Usuario actualizado\n");
+				routingContext.response().setStatusCode(200).putHeader("content-type", "application/json").end(body.encodePrettily());
+			}else {
+				System.out.println("Error al hacer la operación\n");
+				routingContext.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+			}
+			
+		});
+	}
+	
+	private void deleteUserByID(RoutingContext routingContext) {
+		mySQLPool.query("DELETE FROM luzverde.usuario WHERE idUsuario = "+routingContext.request().getParam("idUsuario"), 
+				res -> {
+					if(res.succeeded()) {
+						System.out.println("Usuario borrado\n");
+						routingContext.response().setStatusCode(200).putHeader("content-type", "application/json").end();
+					}else {
+						System.out.println("Error al hacer la operación\n");
+						routingContext.response().setStatusCode(401).putHeader("content-type", "application/json").end(JsonObject.mapFrom(res.cause()).encodePrettily());
+					}
+		});
+	}
 	private void getValueBySensor (RoutingContext routingContext) {
 		mySQLPool.query("SELECT * FROM luzverde.valor_sensor_contaminacion WHERE idSensor = "+ routingContext.request().getParam("idSensor"), 
 				res -> {
